3411b7dbc159cd15e549fe88b0ebf63474621e0d
Hazem Khalil <hazem-khalil@hotmail.com>
2021-05-13 01:32:54 +0200

    Update

--- src/Components/Pdf-view/Pdf.jsx
+++ src/Components/Pdf-view/Pdf.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import LaddPdf from "./ladd.pdf";
-import Bread from "../Bread-crumbs/Bread";
 
 
 const Pdf = () => {
 
     return (
         <div>
-            <Bread/>
             <embed src={LaddPdf} type="application/pdf" style={{width:"100%",height:"100vh"}} />
         </div>
     )
--- src/Components/Pdf-view1/Pdf.jsx
+++ src/Components/Pdf-view1/Pdf.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import faktura from "./faktura.pdf";
-import Bread from "../Bread-crumbs/Bread";
 
 const Pdf = () => {
     return (
         <div>
-            <Bread/>
            <embed src={faktura} type="application/pdf" style={{width:"100%",height:"100vh"}} /> 
         </div>
     )
